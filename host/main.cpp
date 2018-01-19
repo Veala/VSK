02357aa485b87d6fcb66799369e7e0650b82cc47
VealaWork <ialaev86@gmail.com>
2018-01-19 20:42:44 +0300

    next

--- host/main.cpp
+++ host/main.cpp
@@ -7,9 +7,16 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 	cout << "Hello world" << endl;
-	int sfd = socket(AF_INET, SOCK_RAW, tcp);
-	cout << "s: " << s << endl;
+	int sfd = socket(AF_INET, SOCK_STREAM, tcp);
+	if (sfd == -1) {
+		perror("sfd");
+		exit(2);
+	}
+
+	sockaddr_in my_addr;
 	
+	int bnd = bind(sfd, &my_addr, sizeof(sockaddr_in));
+	sock
 
 	string str;
 	cout << "=>";
