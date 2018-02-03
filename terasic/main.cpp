11fce0349477b0a417645957a3ee9d8418599872
Igor Alayev <ialaev86@gmail.com>
2018-02-04 02:12:58 +0300

    into terasic/main.cpp comments header files and deleted using namespace std

--- terasic/main.cpp
+++ terasic/main.cpp
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/select.h>
-#include <sys/time.h>
+//#include <sys/types.h>
+//#include <sys/select.h>
+//#include <sys/time.h>
 #include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+//#include <sys/socket.h>
+//#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string.h>
 
